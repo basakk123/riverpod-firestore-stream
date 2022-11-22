9b0ab83fabcdbc68dd060518ec63ef6423b322d0
basakk123 <basakk@naver.com>
2022-11-22 15:25:50 +0900

    firestore write 하기

---
+++ lib/dto/chat/chat_req_dto.dart
@@ -0,0 +1,27 @@
+class ChatInsertReqDto {
+  String from;
+  String to;
+  String msg;
+
+  // String get from => _from; 게터로 쓸 때
+
+  ChatInsertReqDto({required this.from, required this.to, required this.msg});
+
+  // dart -> map (toJson)
+  Map<String, dynamic> toJson() => {
+        "from": from,
+        "to": to,
+        "msg": msg,
+      };
+
+  // map -> dart (fromJson)
+  factory ChatInsertReqDto.fromJson(Map<String, dynamic> json) => ChatInsertReqDto(
+        from: json["from"],
+        to: json["to"],
+        msg: json["msg"],
+      );
+}
+
+// class ChatDeleteReqDto{
+//
+// }
